cf5e695b639f72cb47f11f15e966487e0f3a994a
Hadi Ahmadi <MacPersia@gmail.com>
2019-06-08 01:33:34 +0800

    making JWT_SECRET_KEY generated at runtime

--- src/main/jib/entrypoint.sh
+++ src/main/jib/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Added by Hadi
+apk update && apk add bash util-linux openssl
+if [ -z "$JWT_SECRET_KEY" ]; then JWT_SECRET_KEY=$(openssl rand -base64 64 | tr '\n' -d); fi
+JAVA_OPTS="${JAVA_OPTS} -Djhipster.security.authentication.jwt.secret=${JWT_SECRET_KEY}"
+#echo "The secret JWT key is ${JWT_SECRET_KEY:?'absent'}."
+
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "be.planty.compare.purchasingpower.PlantyComparePurchasingPowerApp"  "$@"
